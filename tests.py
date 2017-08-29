55530e21874ac8de3c32f27e03df27a6edd77696
Nogmanov Ildar <nogmanov@gmail.com>
2017-08-29 14:14:55 +0300

    Add test

--- tests.py
+++ tests.py
@@ -1,1 +1,10 @@
-# TODO place your tests here
+import unittest
+from format_price import format_price
+
+class TestFormatPrice(unittest.TestCase):
+
+    def test(self):
+        self.assertEqual(format_price(3245.000000), '3 245')
+
+if __name__ == '__main__':
+    unittest.main()
