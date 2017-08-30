55336201b609518b0c6081f16f31c1ce87dc25d0
Nogmanov Ildar <nogmanov@gmail.com>
2017-08-30 06:58:45 +0300

    Add argument parser

--- format_price.py
+++ format_price.py
@@ -1,3 +1,6 @@
+import argparse
+
+
 def format_price(price):
     if type(price) == int:
         return '{0:,}'.format(price).replace(',', ' ')
@@ -5,4 +8,7 @@ def format_price(price):
         return 'Argument must be integer!'
 
 if __name__ == '__main__':
-    pass
+    parser = argparse.ArgumentParser()
+    parser.add_argument("price", type=int, help='Price for formatting')
+    args = parser.parse_args()
+    print(format_price(args.price))
