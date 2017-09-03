d57c16e76ccc369e6e0aae7506e42be40ca7a810
Nogmanov Ildar <nogmanov@gmail.com>
2017-09-03 07:17:53 +0300

    Add try\except statement

--- format_price.py
+++ format_price.py
@@ -2,13 +2,14 @@ import argparse
 
 
 def format_price(price):
-    if type(price) == int:
+    try:
+        price = int(price)
         return '{0:,}'.format(price).replace(',', ' ')
-    else:
-        return 'Argument must be integer!'
+    except ValueError:
+        return None
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument("price", type=int, help='Price for formatting')
+    parser.add_argument("price", help='Price for formatting')
     args = parser.parse_args()
     print(format_price(args.price))
